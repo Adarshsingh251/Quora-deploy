b8a51375c7edc3b85b826a887aed70d3ad84f027
ADARSH5141 <alphasingh81186@gmail.com>
2025-07-27 01:43:01 +0530

    upload quora file

---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config()
+let express = require("express")
+let app = express();
+const { v4: uuidv4 } = require('uuid');
+let path = require("path");
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"/views"))
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({ extended : true}))
+
+let users = [
+    {   
+        id: uuidv4(),
+        username :"Adarsh",
+        posts:"I am learning node.js"
+    },
+    { 
+        id: uuidv4(),
+        username :"sahas",
+        posts:"I am learning backend"
+    },
+    {
+         id: uuidv4(),
+        username :"raj",
+        posts:"I am learning backend in free"
+    }
+]
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{users})
+})
+app.get("/posts/new",(req,res)=>{
+    res.render("new_posts.ejs")
+})
+
+app.post("/posts",(req,res) =>{
+    // console.log(req.body);
+    // res.send("posts send sucessfully")
+    let id = uuidv4();
+    let {username ,posts} = req.body;
+    users.push({ id,username,posts})
+    res.redirect("/posts")
+})
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params
+    let post = users.find(e => id === e.id)
+    res.render("show.ejs",{post})
+})
+app.listen(process.env.PORT,()=>{
+    console.log(`Adarsh port ${process.env.PORT} is listening Continously`);
+})
